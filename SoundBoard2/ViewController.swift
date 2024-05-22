e73ba9a80f6591111b9fbcd6ab0ae93f91c70bb8
Diego Valerio <diego.mamani.co@tecsup.edu.pe>
2024-05-22 11:26:14 -0500

    Initial Commit

---
+++ SoundBoard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard2
+//
+//  Created by Diego Valerio Mamani Coila  on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
